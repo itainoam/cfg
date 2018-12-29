dbd49b4f6b4460ef91abfe4c0b4eb9da447e1e56
itainoam <itai123@gmail.com>
2018-12-29 22:39:11 +0200

    few emacs config updates

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -2,8 +2,20 @@
 (setq mac-option-modifier 'meta)
 (setq auto-save-visited-mode t)
 
+(setq org-startup-indented t
+          org-bullets-bullet-list '("› ")
+          org-ellipsis "  ⌄  "
+          org-pretty-entities t
+          org-hide-emphasis-markers t
+)
+;; sets so documentation pops on the right side
+(set-popup-rule! "^\\*info\\*$" :width 80 :side 'right)
+(set-popup-rule! "^\\*[Hh]elp" :width 120 :side 'right :slot 3)
+(set-popup-rule! "^\\*Man " :width 80 :side 'right)
+(set-popup-rule! "^\\*Customize" :width 80 :side 'right)
+
 (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
-(add-hook 'org-mode-hook 
+(add-hook 'org-mode-hook
           (lambda ()
             (variable-pitch-mode 1)
             (hl-line-mode -1)
