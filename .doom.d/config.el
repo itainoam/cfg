dd1c8df3bc012a8ebe54c17b3a2d515d2807da29
Itai Noam <itai.noam@stratoscale.com>
2018-12-30 14:28:38 +0200

    some more org improvements

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,6 +1,7 @@
 (setq mac-command-modifier 'super)
 (setq mac-option-modifier 'meta)
 (setq auto-save-visited-mode t)
+(set-frame-font "Source Code Pro 14" nil t)
 
 (setq org-startup-indented t
           org-bullets-bullet-list '("› ")
@@ -8,15 +9,22 @@
           org-pretty-entities t
           org-hide-emphasis-markers t
 )
+
 ;; sets so documentation pops on the right side
 (set-popup-rule! "^\\*info\\*$" :width 80 :side 'right)
 (set-popup-rule! "^\\*[Hh]elp" :width 120 :side 'right :slot 3)
 (set-popup-rule! "^\\*Man " :width 80 :side 'right)
 (set-popup-rule! "^\\*Customize" :width 80 :side 'right)
 
+'(org-level-1 ((t (:foreground "#444444" :weight bold :height 1.5))))
+'(org-level-2 ((t (:foreground "#444444" :weight bold :height 1.35))))
+'(org-level-3 ((t (:foreground "#444444" :weight bold :height 1.2))))
+
 (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
 (add-hook 'org-mode-hook
           (lambda ()
-            (variable-pitch-mode 1)
+            (set-window-margins nil 3) ;;requires setting org-mode to apply everytime
             (hl-line-mode -1)
+            (setq smartparens-mode nil)
+            (setq indicate-empty-lines nil)
             (setq line-spacing 0.5)))
