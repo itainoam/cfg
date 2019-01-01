76957d2ed4759e7ef1ee62bac4f29d1cf638dbe2
Itai Noam <itai.noam@stratoscale.com>
2019-01-01 19:02:00 +0200

    more org improvements

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -10,6 +10,23 @@
           org-hide-emphasis-markers t
 )
 
+(setq org-hide-emphasis-markers t)
+
+(defun itai/org-setup ()
+  "Because of autoloading org is not loaded until it's called. It causes some of the variables to not take affect 
+cause org was not loaded.
+with-eval-after-load can be replaced with After!. Which is a macro -> that's why I couldn't find it with spc h f "
+
+  (with-eval-after-load 'org
+    (setq org-startup-indented t
+          org-bullets-bullet-list '("› ")
+          org-ellipsis "  ⌄  "
+          org-pretty-entities t
+          org-hide-emphasis-markers t
+)))
+
+(itai/org-setup)
+
 ;; sets so documentation pops on the right side
 (set-popup-rule! "^\\*info\\*$" :width 80 :side 'right)
 (set-popup-rule! "^\\*[Hh]elp" :width 120 :side 'right :slot 3)
@@ -25,6 +42,7 @@
           (lambda ()
             (set-window-margins nil 3) ;;requires setting org-mode to apply everytime
             (hl-line-mode -1)
+            (setq org-pretty-entities t)
             (setq smartparens-mode nil)
             (setq indicate-empty-lines nil)
             (setq line-spacing 0.5)))
