9728fba32f159cabdc17511b8df4e343ef20c776
itainoam <itai123@gmail.com>
2019-01-05 22:49:32 +0200

    emacs config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,49 +1,54 @@
-(setq mac-command-modifier 'super)
-(setq mac-option-modifier 'meta)
+;; it seems that mac keyboard was fixed in the latest version of doom. can be deleted.
+;;(setq mac-command-modifier 'super)
+;;(setq mac-option-modifier 'meta)
 (setq auto-save-visited-mode t)
 
 (setq doom-font (font-spec :family "Source Code Pro" :size 14))
 (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
 
-(setq org-startup-indented t
-          org-bullets-bullet-list '("› ")
-          org-ellipsis "  ⌄  "
-          org-pretty-entities t
-          org-hide-emphasis-markers t
-)
-
-(setq org-hide-emphasis-markers t)
 
 (defun itai/org-setup ()
   "Because of autoloading org is not loaded until it's called. It causes some of the variables to not take affect 
 cause org was not loaded.
 with-eval-after-load can be replaced with After!. Which is a macro -> that's why I couldn't find it with spc h f "
 
-  (after! 'org
+  (with-eval-after-load 'org
     (setq org-startup-indented t
           org-bullets-bullet-list '("› ")
           org-ellipsis "  ⌄  "
           org-pretty-entities t
           org-hide-emphasis-markers t
-)))
+          )))
 
 (itai/org-setup)
 
+
+
+(map! :map Info-mode-map
+      :n "l" #'evil-forward-char
+      :n "h" #'evil-backward-char
+      :n "C-u" #'Info-scroll-down
+      :n "C-d" #'Info-scroll-up
+      )
+
 ;; sets so documentation pops on the right side
 (set-popup-rule! "^\\*info\\*$" :width 80 :side 'right)
-(set-popup-rule! "^\\*[Hh]elp" :width 120 :side 'right :slot 3)
+(set-popup-rule! "^\\*[Hh]elp" :width 100 :side 'right :slot 3)
 (set-popup-rule! "^\\*Man " :width 80 :side 'right)
 (set-popup-rule! "^\\*Customize" :width 80 :side 'right)
 
-'(org-level-1 ((t (:foreground "#444444" :weight bold :height 1.5))))
-'(org-level-2 ((t (:foreground "#444444" :weight bold :height 1.35))))
-'(org-level-3 ((t (:foreground "#444444" :weight bold :height 1.2))))
+(load-theme 'doom-one-light t)
+(custom-set-faces
+ ;; should be replace with defface or face-remap-add-relative
+ '(org-level-1 ((t (:foreground "#444444" :weight normal :height 1.6))))
+ '(org-level-2 ((t (:foreground "#444444" :weight normal :height 1.4))))
+ '(org-level-3 ((t (:foreground "#444444" :weight normal :height 1.1)))))
 
 (add-hook 'text-mode-hook 'turn-on-visual-line-mode)
 (add-hook 'org-mode-hook
           (lambda ()
             (set-window-margins nil 3) ;;requires setting org-mode to apply everytime
             (hl-line-mode -1)
-            (setq smartparens-mode nil)
+            (smartparens-mode -1) ;; nil toggles and -1 disables
             (setq indicate-empty-lines nil)
             (setq line-spacing 0.5)))
